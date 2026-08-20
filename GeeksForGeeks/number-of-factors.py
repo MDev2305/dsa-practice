f2b4e0c4ca66f4faa9c1a1ac0fb4823fd64d1393
Manisha Singh <mdevspace23@gmail.com>
2026-08-20 13:56:25 +0530

    Create number-of-factors.py

---
+++ GeeksForGeeks/number-of-factors.py
@@ -0,0 +1,12 @@
+class Solution:
+    def countFactors(self, n):
+        count = 0
+
+        for i in range(1, int(n**0.5) + 1):
+            if n % i == 0:
+                count += 1
+
+                if i != n // i:
+                    count += 1
+
+        return count
